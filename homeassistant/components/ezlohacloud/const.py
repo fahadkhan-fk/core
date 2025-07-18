15a936a951f6f15d143983848be1bf7c53e1913f
Fahad Khan <fahad@ezlo.com>
2025-07-18 12:35:52 +0000

    make custom integration work

--- homeassistant/components/ezlohacloud/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Ezlo HA Cloud integration."""
-
-DOMAIN = "ezlohacloud"
-
-STORAGE_KEY = "ezlo_user_data"
-STORAGE_VERSION = 1
-
-EZLO_API_URI = "http://testtunnelezlo.info:8888"
